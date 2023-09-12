9647911d82f01d57b9ec272459df044a47e5e7af
waheedSiddiqui <waheed.siddiqui@redbuffer.net>
2023-09-12 18:48:15 +0500

    separated string of texts into list

--- apps/backend/src/article/article.service.ts
+++ apps/backend/src/article/article.service.ts
@@ -154,7 +154,7 @@ export class ArticleService {
       { populate: ['followers', 'favorites', 'articles'] },
     );
     const article = new Article(user!, dto.title, dto.description, dto.body);
-    article.tagList.push(...dto.tagList);
+    article.tagList = dto.tagList.split(",").map((s) => s.trim());
     user?.articles.add(article);
     await this.em.flush();
 
